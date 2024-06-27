e1cde6804664a6593f58a510c16a807230bae39c
sam9687 <sam968766@gmail.com>
2024-06-27 14:35:02 +0530

    recovered

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+
+export const usernameValidation = z
+        .string()
+        .min(2, "minimum 2 characters required")
+        .max(2, "maximum 20 characters only")
+        .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain any special charactes")
+
+        export const signUpSchema = z.object(
+            {
+                username : usernameValidation,
+                email : z.string().email({message : "Invalid email address"})
+            }
+        )
