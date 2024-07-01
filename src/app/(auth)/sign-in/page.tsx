bfa77dfe3aaf0be9e34b6b3ac91cfcd53db07cf7
sam9687 <sam968766@gmail.com>
2024-07-01 11:03:51 +0530

    sign-in completed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,130 @@
-import React from 'react'
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import { signIn } from "next-auth/react";
 
-export default function SigninPage() {
+import { useToast } from "@/components/ui/use-toast";
+import { redirect, useRouter } from "next/navigation";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { Resolver } from "dns";
+import { signInSchema } from "@/schemas/signInSchema";
+
+// all the necessary imports
+
+export default function SignInPage() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  // this is useEffect hook w
+
+  const onsubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    if (result?.error) {
+      toast({
+        title : "Login Failed",
+        description : "Incorrect credentials",
+        variant : "destructive"
+
+      })
+    }
+    if (result?.url) {
+      router.replace('/')
+    }
+  };
+
   return (
-    <div>SigninPage</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Let's generate energy !!{" "}
+          </h1>
+          <p className="mb-4">Verify yourself </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onsubmit)} className="space-y-6">
+            {/* email form control goes here */}
+
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* password fiels goes here */}
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait....
+                </>
+              ) : (
+                "Signin"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="tex text-center mt-4">
+          <p>
+            Not a member?
+            <Link href="/sign-up" className="text-blue-500 hover:text-blue-800">
+              Signup
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
